1c90325ec470bbdf9fc0ffe94051acad1cfda786
AppleSean <seanzhang2001@icloud.com>
2018-01-16 11:49:55 -0500

    Added methods in world class

--- src/dev/CodeRulers/world/World.java
+++ src/dev/CodeRulers/world/World.java
@@ -15,12 +15,61 @@ import dev.CodeRulers.entity.Peasant;
  * @author seanzhang
  */
 public class World {
+    //these are all the knights that exist in the game.
     private static Knight[] knights;
+    //these are all the peasants that exist in the game.
     private static Peasant[] peasants;
+    //these are all the castles that exist in the game.
     private static Castle[] castles;
     
-    public static Entity[] getEntities() {
-        
-        
+    
+    
+    
+    public static void setKnights(Knight[] knights) {
+        World.knights = knights;
     }
+
+    public static void setPeasants(Peasant[] peasants) {
+        World.peasants = peasants;
+    }
+    
+    public static Knight[] getKnights() {
+        return knights;
+    }
+
+    public static Peasant[] getPeasants() {
+        return peasants;
+    }
+
+    public static Castle[] getCastles() {
+        return castles;
+    }
+    
+    public static String getEntityAt(int x, int y) {
+        if(x>63 || y>63 || x<0 || y<0) {
+            return "Invalid Coordinate";
+        }
+        
+        for(int i=0;i<knights.length;i++) {
+            if(knights[i].getX()==x && knights[i].getY()==y) {
+                return "Knight";
+            }
+        }
+        
+        for(int i=0;i<peasants.length;i++) {
+            if(peasants[i].getX()==x && peasants[i].getY()==y) {
+                return "Peasant";
+            }
+        }
+        
+        for(int i=0;i<castles.length;i++) {
+            if(castles[i].getX()==x && castles[i].getY()==y) {
+                return "Castle";
+            }
+        }
+        
+        return "No Entity";
+    }
+    
+    
 }
