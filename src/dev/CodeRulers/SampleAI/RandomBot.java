b735b2e1e188350c59dc4f3a653829e1cd0fe0f0
335550372 <335550372@109R030573WBQ22.EDU.YRDSB.NET>
2018-01-17 12:06:25 -0500

    added sampleAI package and randombot class

--- src/dev/CodeRulers/SampleAI/RandomBot.java
+++ src/dev/CodeRulers/SampleAI/RandomBot.java
@@ -11,21 +11,47 @@ import java.util.Random;
  * @author 335550372
  */
 public class RandomBot extends AbstractRuler{
-    Random r = new Random();
     
     public RandomBot(int rulerID) {
         super(rulerID);
     }
     @Override
     public void orderSubjects() {
-        
+        Random r = new Random();
+        //Knight:
+
+        for (int i = 0; i < this.getKnights().length; i ++){
+            int randomKnightMove = r.nextInt(2);
+            int knightDir = r.nextInt(7) + 1;
+            if (randomKnightMove == 1){           
+            this.move(this.getKnights()[i], knightDir);
+            }
+            else if (randomKnightMove == 2){
+                this.capture(this.getKnights()[i], knightDir);
+            }
+            else{
+                //do nothing
+            }
+        }
         /*
-            //Knight:
                 Generate 0-2. 0 = Stay. 1 = Move. 2 = Capture.
-                Generate a number 0-7. 0 = N 1 = NE and so forth...
-            //Peasant:
+                Generate a number 1-8. 0 = N 1 = NE and so forth...
+        */
+        //Peasant:
+            for (int i = 0; i < this.getPeasants().length; i ++){
+            int randomPeasantMove = r.nextInt(1);
+            if(randomPeasantMove == 1){
+            int peasantDir = r.nextInt(7) + 1;
+            this.move(this.getPeasants()[i], peasantDir);
+            }
+            else{
+                //Does nothing so peasant stays
+            }
+        }
+            
+        /*
                 Generate 0-1. 0 = Stay. 1 = Move
-                Generate a number 0-7. 0 = N 1 = NE and so forth... 
+                Generate a number 1-8. 0 = N 1 = NE and so forth... 
         */
     }
     @Override
