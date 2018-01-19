844b03bbb57cd1bb2cf7b9fe736f606ec7b9c11f
070035266 <070035266@109R030573Z9Q22.EDU.YRDSB.NET>
2018-01-19 12:05:27 -0500

    started work on Bully AI

---
+++ src/dev/CodeRulers/SampleAI/Bully.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev.CodeRulers.SampleAI;
+
+import dev.CodeRulers.ruler.*;
+
+/**
+ *
+ * @author Luke Klassen
+ */
+public class Bully extends AbstractRuler {
+    
+    //represents how much this ruler is being threatened by nearby Knights
+    int danger = 0;
+    //
+    @Override
+    public void initialize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    @Override
+    public void orderSubjects() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+
+    @Override
+    public String getSchoolName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getRulerName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
