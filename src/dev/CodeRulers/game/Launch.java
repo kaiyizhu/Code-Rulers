0000938a793e44332075ee23691fe82ec9905374
335550372 <335550372@109R030573WBQ22.EDU.YRDSB.NET>
2018-01-22 11:49:25 -0500

    i just comitted for fun lol?

--- src/dev/CodeRulers/game/Launch.java
+++ src/dev/CodeRulers/game/Launch.java
@@ -19,9 +19,7 @@ public class Launch {
     //creates new game object
     
         //this statement is just for testing's sake.
-        AbstractRuler[] r = {new SeanZhang(),new RandomBot(),new SeanZhang(),new SeanZhang(),
-            new SeanZhang(),new SeanZhang(),new SeanZhang(),new SeanZhang(),new SeanZhang(),
-            new SeanZhang(),new SeanZhang(),new SeanZhang()};
+        AbstractRuler[] r = {new SeanZhang(),new RandomBot()};
         
         CodeRulers game = new CodeRulers(r);
         //starts the thread
