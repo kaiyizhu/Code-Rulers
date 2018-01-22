4b07e3baef851aab3357352c31df1eeb27101c08
Sean Zhang <seanzhang2001@gmail.com>
2018-01-22 03:22:54 -0500

    More comments to world class

--- src/dev/CodeRulers/world/World.java
+++ src/dev/CodeRulers/world/World.java
@@ -19,25 +19,40 @@ import java.util.Random;
 
 /**
  * This Class contains all the entities on the world.
- * 
+ *
  * @author Sean Zhang
  */
 public class World {
 
     //this is the xOffset and yOffset that allows for the capability of panning
     //in the world.
-    private static int xOffset=0,yOffset=0;
-    
+    private static int xOffset = 0, yOffset = 0;
+
     //scale factor for world so that we can zoom in and out.
     private static double scaleFactor;
-    
+
     //image for the background of the world
     public static BufferedImage worldMap;
 
+    //the game object
+    private static CodeRulers r;
+
+    //these are all the knights that exist in the game.
+    private static Knight[] knights;
+    //these are all the peasants that exist in the game.
+    private static Peasant[] peasants;
+    //these are all the castles that exist in the game.
+    private static Castle[] castles;
+
+    //these are all the land owned by the ruler.
+    private static int[][] landOwned;
+    
     /**
-     * The Constructor for the World Class. This constructor initializes all the 
+     * The Constructor for the World Class. This constructor initializes all the
      * entities and land tiles in the world.
-     * @param r the Game object that is passed in for access to the ruler objects.
+     *
+     * @param r the Game object that is passed in for access to the ruler
+     * objects.
      */
     public World(CodeRulers r) {
         //creates an empty array of null knights.
@@ -54,10 +69,10 @@ public class World {
 
         //stores the reference of the game object in a variable local to this class.
         this.r = r;
-        
+
         //the scale factor of the map initally starts out as 1 (default)
-        scaleFactor=1;
-        
+        scaleFactor = 1;
+
         //this method intializes all the entities in the world.
         initGame();
     }
@@ -66,90 +81,111 @@ public class World {
      * This method initializes all the entities in the world.
      */
     private static void initGame() {
-        
+
         /*  this for loop goes through every array inside the 2D arrayList,
             landOwned, and fills it with -1, signifying that no one has
             ownership of the land.
-        */
-        for(int[] subLandOwned : landOwned) {
+         */
+        for (int[] subLandOwned : landOwned) {
             Arrays.fill(subLandOwned, -1);
         }
-        
+
         //creates a random object to later use to randomly assign the (x , y)
         //coordinates of the entites in the game.
         Random rand = new Random();
-        
+
         /*  
             Expands the arrays by how many AIs there will be. Since each AI starts
             with 10 knights, the size of the knights array to start with will be
             the size of the ruler array multiplied by 10.
-        */
+         */
         knights = Arrays.copyOf(knights, 10 * r.getRulerArray().length);
-        
+
         /*
             Expands the arrays by how many AIs there will be. Since each AI starts
             with 10 peasants, the size of the peasants array to start with will 
             be the size of the ruler array multiplied by 10.
-        */
+         */
         peasants = Arrays.copyOf(peasants, 10 * r.getRulerArray().length);
-        
+
         /*
             Expands the arrays by how many AIs there will be. Since each AI starts
             with 1 castle, the size of the castles array to start with will be
             the size of the ruler array.
-        */
+         */
         castles = Arrays.copyOf(castles, r.getRulerArray().length);
-        
+
         //count for keeping track on the total number of entites added to the array.
         int count = 0;
-        
+
         /*
             This for-loop goes through all the rulers in the ruler array. It then 
             initializes 10 knights and peasants in random x,y coordinates in the 
             world and 1 castle in a random x,y coordinate in the world. Each tile
             an entity is on to begin with will be assigned ownership to the ruler
             in control of that entity.
-        */
+         */
         for (int i = 0; i < r.getRulerArray().length; i++) {
+            //this for loop iterates 10 times to create 10 peasants and knights.
             for (int j = 0; j < 10; j++) {
+                //finds a random integer between 0 and 64 for both x and y to assign
+                //to an entity in the world. 
                 int rndX = rand.nextInt(64), rndY = rand.nextInt(64);
+
+                //until the random allocation finds an empty spot to claim, it
+                //will continue to randomly find an empty tile to assign.
                 while (landOwned[rndX][rndY] != -1) {
                     rndX = rand.nextInt(64);
                     rndY = rand.nextInt(64);
                 }
+
+                /*
+                    creates a new peasant in the next vacant spot in the peasants
+                    array. The land that the peasant is on is then assinged possesion
+                    by the ruler controlling that peasant.
+                 */
                 peasants[count] = new Peasant(rndX, rndY, i);
                 landOwned[peasants[count].getX()][peasants[count].getY()] = i;
+
+                //until the random allocation finds an empty spot to claim, it
+                //will continue to randomly find an empty tile to assign.
                 while (landOwned[rndX][rndY] != -1) {
                     rndX = rand.nextInt(64);
                     rndY = rand.nextInt(64);
                 }
+
+                /*
+                    creates a new knight in the next vacant spot in the knights
+                    array. The land that the knight is on is then assinged possesion
+                    by the ruler controlling that knight.
+                 */
                 knights[count] = new Knight(rndX, rndY, i);
                 landOwned[knights[count].getX()][knights[count].getY()] = i;
+
+                //add one to the size of the new peasant and knight array.
                 count++;
             }
+
+            //finds a random integer between 0 and 64 for both x and y to assign
+            //to an entity in the world. 
             int rndX = rand.nextInt(64), rndY = rand.nextInt(64);
+
+            //until the random allocation finds an empty spot to claim, it
+            //will continue to randomly find an empty tile to assign.
             while (landOwned[rndX][rndY] != -1) {
                 rndX = rand.nextInt(64);
                 rndY = rand.nextInt(64);
             }
+
+            /*
+                creates a new castle in the next vacant spot in the castle
+                array. The land that the castle is on is then assinged possesion
+                by the ruler controlling that castle.
+             */
             castles[i] = new Castle(rndX, rndY, i);
             landOwned[castles[i].getX()][castles[i].getY()] = i;
         }
-
     }
-
-    //the game object
-    private static CodeRulers r;
-
-    //these are all the knights that exist in the game.
-    private static Knight[] knights;
-    //these are all the peasants that exist in the game.
-    private static Peasant[] peasants;
-    //these are all the castles that exist in the game.
-    private static Castle[] castles;
-
-    //these are all the land owned by the ruler.
-    private static int[][] landOwned;
 
     /**
      * Get method that gets the 2D map of which ruler owns which tile.
@@ -159,7 +195,11 @@ public class World {
     public static int[][] getLandOwned() {
         return landOwned;
     }
-
+    
+    /**
+     * 
+     * @param knights 
+     */
     public static void setKnights(Knight[] knights) {
         World.knights = knights;
     }
@@ -213,26 +253,21 @@ public class World {
     }
 
     public static BufferedImage mapResized;
-    private static double lastScaleFactor=scaleFactor;
-    
+    private static double lastScaleFactor = scaleFactor;
+
     public static void render(Graphics g, CodeRulers r) {
-        
-        
-        
+
         if (worldMap == null) {
-            worldMap = IMAGE.getResizedImage(IMAGE.getBufferedImage("src/resources/images/codeRulersTerrain.png"), (int)(768*scaleFactor), (int)(768*scaleFactor));
+            worldMap = IMAGE.getResizedImage(IMAGE.getBufferedImage("src/resources/images/codeRulersTerrain.png"), (int) (768 * scaleFactor), (int) (768 * scaleFactor));
         }
-        if (mapResized==null) {
-            BufferedImage mapResized=worldMap;
+        if (mapResized == null) {
+            BufferedImage mapResized = worldMap;
         }
-        
-        
-        
-        if(lastScaleFactor!=scaleFactor) {
-            mapResized = IMAGE.getResizedImage(worldMap, (int)(768*scaleFactor), (int)(768*scaleFactor));
+
+        if (lastScaleFactor != scaleFactor) {
+            mapResized = IMAGE.getResizedImage(worldMap, (int) (768 * scaleFactor), (int) (768 * scaleFactor));
         }
-        
-        
+
         g.drawImage(mapResized, xOffset, yOffset, null);
 
         for (int i = 0; i < landOwned[0].length; i++) {
@@ -242,25 +277,25 @@ public class World {
                 } else {
                     Color c = r.getRulerArray()[landOwned[i][j]].getColor();
                     g.setColor(new Color(c.getRed(), c.getGreen(), c.getBlue(), 150));
-                    g.fillRect((int)(i * 12*scaleFactor)+xOffset,(int)( j * 12*scaleFactor)+yOffset,(int)( 12*scaleFactor),(int)( 12*scaleFactor));
+                    g.fillRect((int) (i * 12 * scaleFactor) + xOffset, (int) (j * 12 * scaleFactor) + yOffset, (int) (12 * scaleFactor), (int) (12 * scaleFactor));
                 }
 
             }
         }
 
         for (Peasant p : peasants) {
-            p.drawEntity(g,scaleFactor,xOffset,yOffset);
+            p.drawEntity(g, scaleFactor, xOffset, yOffset);
         }
 
         for (Knight k : knights) {
-            k.drawEntity(g,scaleFactor,xOffset,yOffset);
+            k.drawEntity(g, scaleFactor, xOffset, yOffset);
         }
 
         for (Castle c : castles) {
-            c.drawEntity(g,scaleFactor,xOffset,yOffset);
+            c.drawEntity(g, scaleFactor, xOffset, yOffset);
         }
-        
-        lastScaleFactor=scaleFactor;
+
+        lastScaleFactor = scaleFactor;
     }
 
     /**
@@ -306,6 +341,5 @@ public class World {
     public static void setyOffset(int yOffset) {
         World.yOffset = yOffset;
     }
-    
-    
+
 }
