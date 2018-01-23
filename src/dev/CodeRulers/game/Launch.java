982dcc184eb9f94d142cc93e0e0b3e133f32f097
335550372 <335550372@109R030573WBQ22.EDU.YRDSB.NET>
2018-01-23 10:54:05 -0500

    Merge origin/master
    Conflicts:
    	src/dev/CodeRulers/game/Launch.java

--- src/dev/CodeRulers/game/Launch.java
+++ src/dev/CodeRulers/game/Launch.java
@@ -5,6 +5,7 @@
  */
 package dev.CodeRulers.game;
 
+import dev.CodeRulers.SampleAI.AttackBot;
 import dev.CodeRulers.SampleAI.BullyBot;
 import dev.CodeRulers.SampleAI.RandomBot;
 import dev.CodeRulers.SampleAI.SeanZhang;
@@ -13,14 +14,14 @@ import dev.CodeRulers.ruler.AbstractRuler;
 
 /**
  *
- * @author seanz
+ * @author seanzhang
  */
 public class Launch {
     public static  void main(String[]args) {
     //creates new game object
     
         //this statement is just for testing's sake.
-        AbstractRuler[] r = {new SeanZhang(),new RandomBot()};
+        AbstractRuler[] r = {new SeanZhang(),new AttackBot(), new RandomBot()};
         
         CodeRulers game = new CodeRulers(r);
         //starts the thread
